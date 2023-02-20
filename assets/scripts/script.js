45fb49d12e9b8d242761d852fd6191a67b60b4e7
Potter J. Edgerton <106568662+potteredgerton@users.noreply.github.com>
2023-02-19 20:58:49 -0500

    Create script.js

---
+++ assets/scripts/script.js
@@ -0,0 +1,10 @@
+var container = document.querySelector(".container");
+var taskbar = document.querySelector(".taskbar");
+var startButton = document.querySelector(".startbutton");
+var startMenu = document.querySelector(".startMenu");
+
+function init() {
+  alert("Hello, World!");
+}
+
+document.addEventListener("load", init);
